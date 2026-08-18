2b28d46a2d8beddc4bc46869b05fafa036562d93
Prince gond <princegondrw123@gmail.com>
2026-08-18 08:17:43 +0530

    ,,.

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 
 // ✅ CORS
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174','http://localhost:5175'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
